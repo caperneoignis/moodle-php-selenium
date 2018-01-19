b999cbf452ec00965648bd65e027c141d5aa6939
Lee Kirkland <weeman.kirkland@gmail.com>
2018-01-18 20:32:12 -0500

    got selenium starting along with web server
    Selenium is starting along with webserver, we will see how this stands up to the docker runner.

--- entry_point.sh
+++ entry_point.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+#!/usr/bin/env bash
 
+umask 022
 
+#do a string replace for configuration files for the web root. 
+if [[ "${APACHE_WEB_ROOT}" != "" ]]; then
+    echo "web site will be set to ${APACHE_WEB_ROOT}"
+else
+    #set a default if one is not present
+    APACHE_WEB_ROOT="/var/www/html"    
+fi
+
+sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_WEB_ROOT}#" /etc/apache2/sites-enabled/000-default.conf
+sed -i "s#%%APACHE_WEB_ROOT%%#${APACHE_WEB_ROOT}#" /etc/apache2/apache2.conf
+	
+	
 export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
 function get_server_num() {
@@ -11,14 +25,21 @@ function get_server_num() {
 SELENIUM_PORT=4444;
 
 if [[ "${NUM_OF_SELENIUMS}" != "" ]]; then
+	echo "${NUM_OF_SELENIUMS} will run"
 else
    NUM_OF_SELENIUMS=1;
 fi
-
+NUM_OF_SELENIUMS=$((4444 + NUM_OF_SELENIUMS));
 #we don't want to see the output we just want these to be up. 
-for (( port=4444; port<$NUM_OF_SELENIUMS; port++))
+for ((port=4444; port < NUM_OF_SELENIUMS; port++))
 do
 xvfb-run --auto-servernum --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
   java -jar /opt/selenium/selenium-server-standalone.jar \
-  -port $port > /dev/null 2>&1 &
+  -port $port  > /dev/null 2>&1 &
 done
+
+if [[ $# -eq 1 && $1 == "bash" ]]; then
+	$@;
+else
+	exec "$@";
+fi
